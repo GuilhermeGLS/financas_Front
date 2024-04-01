e438c306c52189078ef6fed9310ea7adedebbc86
Guilherme Lima e Silva <guilhermelimaesilva.p@gmail.com>
2024-04-01 17:51:30 -0300

    Iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { AppRoutes } from "./routes"
+import './assets/global.css'
+
+export const App = () => {
+ 
+
+  return (
+    <AppRoutes />
+  )
+}
+
+
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+
+
+export const Header = () => {
+    return(
+        <header>
+            <h2>Controle Financeiro</h2>
+        </header>
+    )
+}
